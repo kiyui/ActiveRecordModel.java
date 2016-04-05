db7cda236da1412d6d26211f6775a3fd50730be7
Timur Lavrenti Kiyivinski <deathseclipse@gmail.com>
2016-04-05 15:23:53 +0800

    [WIP] Database connection and SELECT queries.

--- src/com/Kiyivinski/ActiveRecordModel.java
+++ src/com/Kiyivinski/ActiveRecordModel.java
@@ -1,8 +1,10 @@
 package com.Kiyivinski;
 
-import com.sun.org.apache.xpath.internal.operations.Bool;
+import java.sql.*;
+import org.mariadb.jdbc.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class ActiveRecordModel {
     private static String hostname;
@@ -10,6 +12,7 @@ public class ActiveRecordModel {
     private static String password;
     private static String database;
     private static Integer port;
+    private static Connection connection;
     private String table;
     private ArrayList<String> columns;
     private Boolean verbose;
@@ -24,17 +27,68 @@ public class ActiveRecordModel {
         this(table, columns, false);
     }
 
+    public ArrayList<String> getColumns() {
+        return this.columns;
+    }
+
     public Boolean connect(String hostname, String username, String password, String database, Integer port) {
         this.hostname = hostname;
         this.username = username;
         this.password = password;
         this.database = database;
         this.port = port;
-        return true;
+        this.connection = null;
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+            String address = "jdbc:mariadb://" + this.hostname
+                    + ":" + this.port
+                    + "/" + this.database
+                    + "?user=" + this.username
+                    + "&password=" + this.password;
+            if (this.verbose)
+                System.out.println("Using connection address: " + address);
+            this.connection = DriverManager.getConnection(address);
+            return true;
+        } catch(SQLException e) {
+            if (this.verbose)
+                System.out.println("SQL Connection error.");
+            return false;
+        } catch (Exception e) {
+            if (this.verbose)
+                System.out.println("Unknown error at `connect()`.");
+            return false;
+        }
     }
 
     public Boolean connect(String hostname, String username, String password, String database) {
         return connect(hostname, username, password, database, 3306);
+    }
+
+    public ArrayList<HashMap<String, String>> all() throws SQLException {
+        try {
+            Statement statement =  this.connection.createStatement();
+
+            String sql = "SELECT * from " + this.table;
+            ResultSet resultSet = statement.executeQuery(sql);
+
+            ArrayList<HashMap<String, String>> results = new ArrayList<>();
+            while(resultSet.next()) {
+                HashMap<String, String> result = new HashMap<>();
+                for (String column: this.columns) {
+                    result.put(column, resultSet.getString(column));
+                    if (this.verbose)
+                        System.out.print(resultSet.getString(column) + ' ');
+                }
+                if (this.verbose)
+                    System.out.println("");
+                results.add(result);
+            }
+            return results;
+        } catch (SQLException e) {
+            if (this.verbose)
+                System.out.println("Caught SQL error at `all()`.");
+            throw e;
+        }
     }
 }
 
--- src/com/Kiyivinski/Main.java
+++ src/com/Kiyivinski/Main.java
@@ -2,24 +2,39 @@ package com.Kiyivinski;
 
 import com.Kiyivinski.ActiveRecordModel;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<String> jobColumns = new ArrayList<String>();
+        ArrayList<String> jobColumns = new ArrayList<>();
         jobColumns.add("id");
         jobColumns.add("job");
         jobColumns.add("pay");
 
-        ArrayList<String> employeeColumns = new ArrayList<String>();
+        ArrayList<String> employeeColumns = new ArrayList<>();
         employeeColumns.add("id");
         employeeColumns.add("first");
         employeeColumns.add("last");
 
-	    ActiveRecordModel jobModel = new ActiveRecordModel("jobs", jobColumns);
-        ActiveRecordModel employeeModel = new ActiveRecordModel("employees", employeeColumns);
+	    ActiveRecordModel jobModel = new ActiveRecordModel("jobs", jobColumns, true);
+        ActiveRecordModel employeeModel = new ActiveRecordModel("Employees", employeeColumns);
 
-        jobModel.connect("127.0.0.1", "root", "password", "java");
+        if(jobModel.connect("127.0.0.1", "root", "password", "java", 3306)) {
+            try {
+                ArrayList<HashMap<String, String>> results =  employeeModel.all();
+                for (HashMap<String, String> result: results) {
+                    System.out.println("Employee: " + result.get("first") + " registered.");
+                }
+            } catch (SQLException e) {
+                System.out.println("Error connecting to SQL server");
+            }
+        }
+        else {
+            System.out.println("Error");
+        }
+
     }
 }
